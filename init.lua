29e676e33d0ccf7a243a0865436a35cf9c24eea1
PeterALX <quaypeter25@gmail.com>
2023-03-22 09:43:45 +0000

    Blazingly fastgit add .

---
+++ init.lua
@@ -0,0 +1,8 @@
+
+require("myconfig.set")
+require("myconfig.remap")
+require("myconfig.packer")
+require("myconfig.plugin_config.treesitter")
+require("myconfig.plugin_config.lsp_zero")
+require("myconfig.plugin_config.toggleterm")
+require("myconfig.plugin_config.nvim-tree")
---
+++ lua/myconfig/set.lua
@@ -0,0 +1,18 @@
+--disable netrw at the start. strongly advised by vimtree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
