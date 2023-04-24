4337402b7822eb2d8e62c7b4efba0b9318b5f56c
NinnjA254 <72952862+NinnjA254@users.noreply.github.com>
2023-04-24 23:39:29 +0300

    Update windline.lua

--- lua/myconfig/plugin_config/windline.lua
+++ lua/myconfig/plugin_config/windline.lua
@@ -1,1 +1,7 @@
 require('wlsample.airline')
+require('wlsample.airline_anim')
+require('wlsample.airline_luffy')
+-- only support linux it need install cava
+lua require("windline.components.cava").toggle()
+require('wlsample.vscode')
+require('wlsample.evil_line')
