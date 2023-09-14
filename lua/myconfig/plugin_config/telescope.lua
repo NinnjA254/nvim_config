02c7e7bc144353203b4c0308bd9132012b4d39f3
PeterALX <quaypeter25@gmail.com>
2023-09-14 14:02:23 +0000

    trying somn

--- lua/myconfig/plugin_config/telescope.lua
+++ lua/myconfig/plugin_config/telescope.lua
@@ -1,7 +1,18 @@
 require("telescope").setup {
-	defaults = {
-		file_ignore_patterns = {
-			"node_modules"
-		}
-	}
+	-- defaults = {
+	-- 	file_ignore_patterns = {
+	-- 		"node_modules"
+	-- 	}
+	-- },
+	vimgrep_arguments = {
+		'rg',
+		'--color=never',
+		'--no-heading',
+		'--with-filename',
+		'--line-number',
+		'--column',
+		'--smart-case',
+		'--ignore-file',
+		'.gitignore'
+	},
 }
